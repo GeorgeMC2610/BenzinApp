d268f0bc44589c5024ebedaad8af0583f5301fe2
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-03-28 21:25:34 +0300

    added button

--- app/src/main/java/com/georgemc2610/benzinapp/ActivityAddRecord.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityAddRecord.java
@@ -1,6 +1,7 @@
 package com.georgemc2610.benzinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.Debug;
 
 import android.os.Bundle;
 
@@ -12,7 +13,18 @@ public class ActivityAddRecord extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
+
+        // action bar with back button.
+        try
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Something went wrong while trying to find Action Bar. Message: " + e.getMessage());
+        }
     }
 
-    
+
 }
--- app/src/main/java/com/georgemc2610/benzinapp/ui/history/HistoryFragment.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ui/history/HistoryFragment.java
@@ -8,18 +8,24 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.georgemc2610.benzinapp.R;
 import com.georgemc2610.benzinapp.databinding.FragmentHistoryBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HistoryFragment extends Fragment
 {
 
     private FragmentHistoryBinding binding;
 
+    FloatingActionButton ButtonAdd;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
         binding = FragmentHistoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        ButtonAdd = root.findViewById(R.id.button_add);
 
         return root;
     }
