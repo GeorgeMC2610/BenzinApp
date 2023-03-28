f5d3680b2eb4dc3104a581c86f0c306a0ac82ea1
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-03-28 23:10:03 +0300

    added timestamp in the code

--- app/src/main/java/com/georgemc2610/benzinapp/DatabaseManager.java
+++ app/src/main/java/com/georgemc2610/benzinapp/DatabaseManager.java
@@ -12,6 +12,7 @@ public class DatabaseManager
     private DatabaseManager(SQLiteDatabase DB)
     {
         this.DB = DB;
+        //DB.execSQL("DROP TABLE IF EXISTS BENZINAPP;");
         DB.execSQL("CREATE TABLE IF NOT EXISTS BENZINAPP (" +
                 "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                 "liters REAL NOT NULL," +
@@ -19,7 +20,8 @@ public class DatabaseManager
                 "kilometers REAL NOT NULL," +
                 "lt_per_hundred REAL NOT NULL," +
                 "km_per_lt REAL NOT NULL," +
-                "eur_per_km REAL NOT NULL)");
+                "eur_per_km REAL NOT NULL," +
+                "timestamp DATETIME NOT NULL);");
     }
 
     public static DatabaseManager getInstance(@Nullable SQLiteDatabase DB)
