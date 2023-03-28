ae50e43db40ac1da0e3215d1020cff834bf534d0
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-03-28 21:15:01 +0300

    added new activity

---
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityAddRecord.java
@@ -0,0 +1,18 @@
+package com.georgemc2610.benzinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAddRecord extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_record);
+    }
+
+    
+}
