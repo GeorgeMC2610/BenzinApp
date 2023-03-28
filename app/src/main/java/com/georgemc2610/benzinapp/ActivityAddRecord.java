897899a357b834abe2e333a2d683d2162c97d017
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-03-28 22:18:07 +0300

    added constraints in button
    - now all fields must be filled

--- app/src/main/java/com/georgemc2610/benzinapp/ActivityAddRecord.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityAddRecord.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.motion.widget.Debug;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityAddRecord extends AppCompatActivity
 {
+    EditText editTextLiters;
+    EditText editTextCost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // initialize activity.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
+
+        // get edit texts.
+        editTextLiters = findViewById(R.id.editTextLiters);
+        editTextCost = findViewById(R.id.editTextCost);
 
         // action bar with back button and correct title name.
         try
@@ -28,5 +38,28 @@ public class ActivityAddRecord extends AppCompatActivity
         }
     }
 
+    public void OnButtonAddClicked(View v)
+    {
+        int validation_counter = 0;
 
+        // if either of the edit texts are empty, don't proceed.
+        if (editTextCost.getText().toString().length() == 0)
+        {
+            editTextCost.setError(getResources().getString(R.string.error_field_cannot_be_empty));
+            validation_counter--;
+        }
+
+        if (editTextLiters.getText().toString().length() == 0)
+        {
+            editTextLiters.setError(getResources().getString(R.string.error_field_cannot_be_empty));
+            validation_counter--;
+        }
+
+        // there must be exactly two correct validations to proceed.
+        if (validation_counter != 0)
+            return;
+
+        // proceed to add properties.
+        Toast.makeText(this, "Adding Record...", Toast.LENGTH_LONG).show();
+    }
 }
