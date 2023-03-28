507ba3281632d17ac66a7d62223866e07b5687b3
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-03-28 21:01:16 +0300

    Create Project

---
+++ app/src/main/java/com/georgemc2610/benzinapp/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,37 @@
+package com.georgemc2610.benzinapp.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.georgemc2610.benzinapp.databinding.FragmentDashboardBinding;
+
+public class DashboardFragment extends Fragment {
+
+    private FragmentDashboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState)
+    {
+        DashboardViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);
+
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView()
+    {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/georgemc2610/benzinapp/ui/home/HomeViewModel.java
@@ -0,0 +1,22 @@
+package com.georgemc2610.benzinapp.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel
+{
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel()
+    {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText()
+    {
+        return mText;
+    }
+}
