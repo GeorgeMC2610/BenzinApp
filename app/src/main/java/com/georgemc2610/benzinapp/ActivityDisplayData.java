31e3ebaa60651fb1161c690fa7d8177af12ac0fc
GeorgeMC2610 <giorgosseimenis@gmail.com>
2023-03-30 11:44:35 +0300

    initializers in display data

--- app/src/main/java/com/georgemc2610/benzinapp/ActivityDisplayData.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityDisplayData.java
@@ -6,15 +6,28 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ActivityDisplayData extends AppCompatActivity
 {
+
+    TextView date, petrolType, cost, liters, kilometers, lt_per_100, km_per_lt, cost_per_km;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_data);
+
+        // initialize views.
+        date = findViewById(R.id.textView_Date);
+        petrolType = findViewById(R.id.textView_PetrolType);
+        cost = findViewById(R.id.textView_Cost);
+        liters = findViewById(R.id.textView_Liters);
+        kilometers = findViewById(R.id.textView_Kilometers);
+        lt_per_100 = findViewById(R.id.textView_LitersPer100Kilometers);
+        km_per_lt = findViewById(R.id.textView_KilometersPerLiter);
+        cost_per_km = findViewById(R.id.textView_CostPerKilometer);
 
         // action bar with back button and correct title name.
         try
