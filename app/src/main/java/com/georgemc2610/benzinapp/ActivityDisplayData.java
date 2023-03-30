36d3a6c08d8fb7d69b7dceedf25d2ccd194a2b12
GeorgeMC2610 <giorgosseimenis@gmail.com>
2023-03-30 09:52:01 +0300

    initialize properties for display data

--- app/src/main/java/com/georgemc2610/benzinapp/ActivityDisplayData.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityDisplayData.java
@@ -2,7 +2,10 @@ package com.georgemc2610.benzinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ActivityDisplayData extends AppCompatActivity
 {
@@ -12,5 +15,31 @@ public class ActivityDisplayData extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_data);
+
+        // action bar with back button and correct title name.
+        try
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle(getString(R.string.title_display_data));
+        }
+        // if anything goes wrong, print it out.
+        catch (Exception e)
+        {
+            System.out.println("Something went wrong while trying to find Action Bar. Message: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
