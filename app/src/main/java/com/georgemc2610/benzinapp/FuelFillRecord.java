280425b77ac53379d75cff2c9438bb5328619b9d
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-04-01 16:00:24 +0300

    this looks just right

--- app/src/main/java/com/georgemc2610/benzinapp/FuelFillRecord.java
+++ app/src/main/java/com/georgemc2610/benzinapp/FuelFillRecord.java
@@ -1,6 +1,39 @@
 package com.georgemc2610.benzinapp;
 
+import java.time.LocalDate;
+
 public class FuelFillRecord
 {
+    private float liters, cost_eur, kilometers;
+    private float gallons, cost_usd, miles;
+    private LocalDate date;
+
+    private final float lt_per_100km, km_per_lt, costEur_per_km;
+    private final float mpg, costUsd_per_mile;
+
+    public FuelFillRecord(float liters, float cost_eur, float kilometers, LocalDate date)
+    {
+        // initialize values
+        this.liters = liters;
+        this.cost_eur = cost_eur;
+        this.kilometers = kilometers;
+        this.date = date;
+
+        // conversions
+        this.miles = (float) (kilometers / 1.609);
+        this.gallons = (float) (liters / 3.785);
+        this.cost_usd = (float) (cost_eur * 1.09);
+
+        // based on the values above,
+        lt_per_100km = 100 * liters / kilometers;
+        km_per_lt = kilometers / liters;
+        costEur_per_km = cost_eur / kilometers;
+
+        // also convert the other values
+        mpg = miles / gallons;
+        costUsd_per_mile = cost_usd / miles;
+    }
+
     
+
 }
