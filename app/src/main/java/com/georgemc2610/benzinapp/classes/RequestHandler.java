62f3f6ddbb7ebd74d9c18e36353370d17b9a578a
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-04-03 01:57:50 +0300

    added volley to this one too

--- app/src/main/java/com/georgemc2610/benzinapp/classes/RequestHandler.java
+++ app/src/main/java/com/georgemc2610/benzinapp/classes/RequestHandler.java
@@ -1,10 +1,20 @@
 package com.georgemc2610.benzinapp.classes;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class RequestHandler
 {
     private static RequestHandler instance;
+    private static final String _URL = "http://192.168.1.86:3000";
+
 
     private String token;
+    private RequestQueue requestQueue;
 
     private RequestHandler()
     {
@@ -17,9 +27,17 @@ public class RequestHandler
         instance = new RequestHandler();
     }
 
-    public void Login(String Username, String Password)
+    public void Login(Context context, String Username, String Password)
     {
+        requestQueue = Volley.newRequestQueue(context);
 
+        String url = _URL + "/auth/login";
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, response -> {}, error -> {})
+        {
+          
+
+        };
     }
 
 }
