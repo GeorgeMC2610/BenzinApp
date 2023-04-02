98d20b3c01b6968b695da7e341e88f2978f8fea1
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-04-03 00:53:46 +0300

    personalized code for the activity

--- app/src/main/java/com/georgemc2610/benzinapp/LoginActivity.java
+++ app/src/main/java/com/georgemc2610/benzinapp/LoginActivity.java
@@ -3,14 +3,31 @@ package com.georgemc2610.benzinapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity
 {
+
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.editText_Username);
+        password = findViewById(R.id.editText_Password);
+
+        login = findViewById(R.id.buttonLogin);
     }
+
+    public void OnButtonLoginPressed(View v)
+    {
+        
+    }
+
 }
