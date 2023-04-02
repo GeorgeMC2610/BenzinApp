2a70544186587c3cb0ad467e6336afbc82a92e9e
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-04-03 01:14:48 +0300

    register activity

---
+++ app/src/main/java/com/georgemc2610/benzinapp/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.georgemc2610.benzinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+}
