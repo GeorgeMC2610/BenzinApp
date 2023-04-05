c4bce6f12735c5fc1739761fd980e1a46a29f55e
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-04-05 21:33:11 +0300

    added strings in login

--- app/src/main/java/com/georgemc2610/benzinapp/RegisterActivity.java
+++ app/src/main/java/com/georgemc2610/benzinapp/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.georgemc2610.benzinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity
 {
@@ -13,6 +15,12 @@ public class RegisterActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+    }
 
+    public void OnTextViewLoginClicked(View v)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
