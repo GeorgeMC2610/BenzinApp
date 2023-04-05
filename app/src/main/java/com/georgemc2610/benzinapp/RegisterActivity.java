7aa07d7059627c5bd7d8e1c4be17941f0dd440bb
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-04-05 22:05:57 +0300

    added checks

--- app/src/main/java/com/georgemc2610/benzinapp/RegisterActivity.java
+++ app/src/main/java/com/georgemc2610/benzinapp/RegisterActivity.java
@@ -32,4 +32,63 @@ public class RegisterActivity extends AppCompatActivity
         startActivity(intent);
         finish();
     }
+
+    public void OnButtonRegisterPressed(View v)
+    {
+        boolean canContinue = true;
+
+        // --- FIELDS EMPTY --- //
+        if (username.getText().toString().trim().length() == 0)
+        {
+            canContinue = false;
+            username.setError(getString(R.string.error_field_cannot_be_empty));
+        }
+
+        if (password.getText().toString().length() == 0)
+        {
+            canContinue = false;
+            password.setError(getString(R.string.error_field_cannot_be_empty));
+        }
+
+        if (passwordConfirmation.getText().toString().length() == 0)
+        {
+            canContinue = false;
+            passwordConfirmation.setError(getString(R.string.error_field_cannot_be_empty));
+        }
+
+        if (manufacturer.getText().toString().trim().length() == 0)
+        {
+            canContinue = false;
+            manufacturer.setError(getString(R.string.error_field_cannot_be_empty));
+        }
+
+        if (model.getText().toString().trim().length() == 0)
+        {
+            canContinue = false;
+            model.setError(getString(R.string.error_field_cannot_be_empty));
+        }
+
+        if (year.getText().toString().trim().length() == 0)
+        {
+            canContinue = false;
+            year.setError(getString(R.string.error_field_cannot_be_empty));
+        }
+
+        // -- PASSWORD CONFIRMATION WRONG -- //
+        if (!passwordConfirmation.getText().toString().equals(password.getText().toString()))
+        {
+            canContinue = false;
+            passwordConfirmation.setError(getString(R.string.error_password_confirmation_wrong));
+        }
+
+        if (!canContinue)
+            return;
+
+        String carManufacturer = manufacturer.getText().toString();
+        String carModel = model.getText().toString();
+        int carYear = Integer.parseInt(year.getText().toString());
+        String Username = username.getText().toString();
+        String Password = password.getText().toString();
+        String PasswordConfirmation = passwordConfirmation.getText().toString();
+    }
 }
