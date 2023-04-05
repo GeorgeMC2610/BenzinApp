195a0c30b5c50074347d0ee198386053068b82c9
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-04-05 21:37:15 +0300

    added edit texts in Register Activity

--- app/src/main/java/com/georgemc2610/benzinapp/RegisterActivity.java
+++ app/src/main/java/com/georgemc2610/benzinapp/RegisterActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity
 {
+    EditText username, password, passwordConfirmation, manufacturer, model, year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,6 +17,13 @@ public class RegisterActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // initialize values
+        username = findViewById(R.id.editText_Username);
+        password = findViewById(R.id.editText_Password);
+        passwordConfirmation = findViewById(R.id.editText_PasswordConfirmation);
+        manufacturer = findViewById(R.id.editText_CarManufacturer);
+        model = findViewById(R.id.editText_CarModel);
+        year = findViewById(R.id.editText_Year);
     }
 
     public void OnTextViewLoginClicked(View v)
