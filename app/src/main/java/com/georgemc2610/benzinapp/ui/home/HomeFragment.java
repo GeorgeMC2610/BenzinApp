efdb50f1fac544e3c75a1a00e0b96b0c36bed5eb
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-04-06 12:20:42 +0300

    added a Graph View!

--- app/src/main/java/com/georgemc2610/benzinapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.georgemc2610.benzinapp.ui.home;
 
+import android.graphics.Color;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,9 @@ import com.android.volley.Response;
 import com.georgemc2610.benzinapp.R;
 import com.georgemc2610.benzinapp.classes.RequestHandler;
 import com.georgemc2610.benzinapp.databinding.FragmentHomeBinding;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,6 +37,31 @@ public class HomeFragment extends Fragment implements Response.Listener<String>
         // get views
         car = root.findViewById(R.id.textView_Car);
         year = root.findViewById(R.id.textView_Year);
+
+        GraphView graphView = (GraphView) root.findViewById(R.id.graph);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(-2, 4),
+                new DataPoint(-1, 1),
+                new DataPoint(0, 0),
+                new DataPoint(1, 1),
+                new DataPoint(2, 4),
+        });
+
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(-2, -2),
+                new DataPoint(-1, -1),
+                new DataPoint(0, 0),
+                new DataPoint(1, 1),
+                new DataPoint(2, 2),
+        });
+
+        series.setColor(Color.rgb(255, 0, 0));
+        series2.setColor(Color.rgb(0, 255, 0));
+
+        graphView.addSeries(series);
+        graphView.addSeries(series2);
+        graphView.setTitle("ΚΑΤΑΝΑΛΩΣΗ");
 
         // make the request
         RequestHandler.getInstance().GetCarInfo(getActivity(), this);
