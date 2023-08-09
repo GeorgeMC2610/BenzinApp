d8faeeb66238e4a1e03983ba3ed270a47dd1cd16
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-08-09 14:05:35 +0300

    getting the fuel fill record

--- app/src/main/java/com/georgemc2610/benzinapp/ActivityEditRecord.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityEditRecord.java
@@ -3,14 +3,22 @@ package com.georgemc2610.benzinapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.georgemc2610.benzinapp.classes.FuelFillRecord;
+
 public class ActivityEditRecord extends AppCompatActivity
 {
+    private FuelFillRecord record;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // necessary code for activity creation.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_record);
+
+        // get the fuel fill record passed to edit.
+        record = (FuelFillRecord) getIntent().getSerializableExtra("record");
     }
 
     
