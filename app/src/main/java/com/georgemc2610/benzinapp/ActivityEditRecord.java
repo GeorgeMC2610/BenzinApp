5598d82350992e37650b5fa274c4299637eb30b2
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-08-10 16:09:02 +0300

    removed imports

--- app/src/main/java/com/georgemc2610/benzinapp/ActivityEditRecord.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityEditRecord.java
@@ -1,21 +1,14 @@
 package com.georgemc2610.benzinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.android.volley.Response;
 import com.georgemc2610.benzinapp.classes.FuelFillRecord;
 import com.georgemc2610.benzinapp.classes.RequestHandler;
-
-import java.time.LocalDate;
 
 // TODO: Add comments.
 
