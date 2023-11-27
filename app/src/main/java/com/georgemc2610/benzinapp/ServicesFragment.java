3b3dc23b020fe3ea7cd80582d5ee2d4fc6bc37d8
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-11-28 00:53:46 +0200

    fragment for interchangable tabs

--- app/src/main/java/com/georgemc2610/benzinapp/ServicesFragment.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ServicesFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.georgemc2610.benzinapp.databinding.FragmentServicesBinding;
-import com.georgemc2610.benzinapp.databinding.FragmentSettingsBinding;
 
 public class ServicesFragment extends Fragment
 {
