d7842cceb6d510ab538e87839b232849b51faf34
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-11-27 18:21:56 +0200

    necessary edits to add new fragment

--- app/src/main/java/com/georgemc2610/benzinapp/MainActivity.java
+++ app/src/main/java/com/georgemc2610/benzinapp/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_all_records, R.id.navigation_settings).build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_all_records, R.id.navigation_services, R.id.navigation_settings).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
