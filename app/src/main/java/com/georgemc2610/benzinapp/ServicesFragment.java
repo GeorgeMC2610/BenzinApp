c65cc70f5aee525d99d7e67e859db0ea302c0128
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-11-28 20:04:23 +0200

    added functionality for tabs!

--- app/src/main/java/com/georgemc2610/benzinapp/ServicesFragment.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ServicesFragment.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.georgemc2610.benzinapp.databinding.FragmentServicesBinding;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
-public class ServicesFragment extends Fragment
+public class ServicesFragment extends Fragment implements TabLayout.OnTabSelectedListener
 {
 
     private FragmentServicesBinding binding;
+    private TabLayout tabLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -21,6 +24,13 @@ public class ServicesFragment extends Fragment
         // Inflate the layout for this fragment
         binding = FragmentServicesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // Get the tab layout.
+        tabLayout = root.findViewById(R.id.tab_layout);
+
+        // Dynamically add two tab items.
+        tabLayout.addOnTabSelectedListener(this);
+        tabLayout.selectTab(tabLayout.getTabAt(1));
 
         return root;
     }
@@ -30,5 +40,23 @@ public class ServicesFragment extends Fragment
     {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab)
+    {
+        System.out.println("TAB SELECTED: " + tab.getText());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab)
+    {
+        System.out.println("TAB UNSELECTED: " + tab.getText());
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab)
+    {
+        System.out.println("TAB RESELECTED: " + tab.getText());
     }
 }
