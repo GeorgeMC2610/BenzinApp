e1ad32baa846911b164a8f411ce52344011551a6
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-11-28 20:24:36 +0200

    added tab layout logic

--- app/src/main/java/com/georgemc2610/benzinapp/ServicesFragment.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ServicesFragment.java
@@ -7,9 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.georgemc2610.benzinapp.databinding.FragmentServicesBinding;
-import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 public class ServicesFragment extends Fragment implements TabLayout.OnTabSelectedListener
@@ -17,6 +18,8 @@ public class ServicesFragment extends Fragment implements TabLayout.OnTabSelecte
 
     private FragmentServicesBinding binding;
     private TabLayout tabLayout;
+    private ScrollView servicesScrollView, malfunctionsScrollView;
+    private LinearLayout servicesLinearLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -28,9 +31,19 @@ public class ServicesFragment extends Fragment implements TabLayout.OnTabSelecte
         // Get the tab layout.
         tabLayout = root.findViewById(R.id.tab_layout);
 
+        // Get the other scroll views.
+        servicesScrollView = root.findViewById(R.id.scroll_view_services);
+        malfunctionsScrollView = root.findViewById(R.id.scroll_view_malfunctions);
+
+        servicesLinearLayout = root.findViewById(R.id.linear_layout_services);
+
+        // inflate sample layouts
+        View v = inflater.inflate(R.layout.cardview_service, null);
+        servicesLinearLayout.addView(v);
+
         // Dynamically add two tab items.
         tabLayout.addOnTabSelectedListener(this);
-        tabLayout.selectTab(tabLayout.getTabAt(1));
+        tabLayout.selectTab(tabLayout.getTabAt(0));
 
         return root;
     }
@@ -45,7 +58,21 @@ public class ServicesFragment extends Fragment implements TabLayout.OnTabSelecte
     @Override
     public void onTabSelected(TabLayout.Tab tab)
     {
-        System.out.println("TAB SELECTED: " + tab.getText());
+        System.out.println("TAB SELECTED: " + tabLayout.getSelectedTabPosition());
+
+        switch (tabLayout.getSelectedTabPosition())
+        {
+            case 0:
+                servicesScrollView.setVisibility(View.GONE);
+                malfunctionsScrollView.setVisibility(View.VISIBLE);
+                break;
+            case 1:
+                servicesScrollView.setVisibility(View.VISIBLE);
+                malfunctionsScrollView.setVisibility(View.GONE);
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
     }
 
     @Override
