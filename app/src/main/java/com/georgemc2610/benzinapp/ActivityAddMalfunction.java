cbb3287f72a53a319771c933f18bec4115af1e5a
GeorgeMC2610 <giorgosseimenis@gmail.com>
2023-12-02 19:15:01 +0200

    fixed ids to retrieve them in the activity

--- app/src/main/java/com/georgemc2610/benzinapp/ActivityAddMalfunction.java
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityAddMalfunction.java
@@ -3,9 +3,14 @@ package com.georgemc2610.benzinapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityAddMalfunction extends AppCompatActivity
 {
+    EditText title, description, atKm;
+    TextView date;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -13,5 +18,11 @@ public class ActivityAddMalfunction extends AppCompatActivity
         // initialize activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_malfunction);
+
+        // retrieve edit texts and text view
+        title = findViewById(R.id.editTextMalfunctionTitle);
+        description = findViewById(R.id.editTextMalfunctionDesc);
+        atKm = findViewById(R.id.editTextMalfunctionAtKm);
+        date = findViewById(R.id.textViewMalfunctionDatePicked);
     }
 }
