66a5dce09a7446b893593364b7ffecc900a0919f
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-12-04 23:06:20 +0200

    refactored a class

--- app/src/main/java/com/georgemc2610/benzinapp/classes/RequestHandler.java
+++ app/src/main/java/com/georgemc2610/benzinapp/classes/RequestHandler.java
@@ -1,9 +1,7 @@
 package com.georgemc2610.benzinapp.classes;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Debug;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -11,17 +9,14 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.georgemc2610.benzinapp.ActivityDisplayData;
 import com.georgemc2610.benzinapp.MainActivity;
 import com.georgemc2610.benzinapp.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
