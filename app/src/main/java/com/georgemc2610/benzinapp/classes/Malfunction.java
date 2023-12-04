543c97e06d0aba5fc4c8a1c7a3120893b393f6ba
GeorgeMC2610 <giorgosseimenis@gmail.com>
2023-12-04 17:41:21 +0200

    response parsing

--- app/src/main/java/com/georgemc2610/benzinapp/classes/Malfunction.java
+++ app/src/main/java/com/georgemc2610/benzinapp/classes/Malfunction.java
@@ -1,5 +1,88 @@
 package com.georgemc2610.benzinapp.classes;
 
+import java.time.LocalDate;
+
 public class Malfunction
 {
+    private int id, at_km;
+    private float cost;
+    private String title, description;
+    private LocalDate started, ended;
+
+    public Malfunction(int id, int at_km, String title, String description, LocalDate started)
+    {
+        this.id = id;
+        this.at_km = at_km;
+        this.title = title;
+        this.description = description;
+        this.started = started;
+    }
+
+    // GETTERS
+    public int getId()
+    {
+        return id;
+    }
+
+    public int getAt_km()
+    {
+        return at_km;
+    }
+
+    public float getCost()
+    {
+        return cost;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public LocalDate getStarted()
+    {
+        return started;
+    }
+
+    public LocalDate getEnded()
+    {
+        return ended;
+    }
+
+    // SETTERS
+
+    public void setAt_km(int at_km)
+    {
+        this.at_km = at_km;
+    }
+
+    public void setCost(float cost)
+    {
+        this.cost = cost;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public void setStarted(LocalDate started)
+    {
+        this.started = started;
+    }
+
+    public void setEnded(LocalDate ended)
+    {
+        this.ended = ended;
+    }
 }
--- app/src/main/java/com/georgemc2610/benzinapp/classes/listeners/ResponseMalfunctionListener.java
+++ app/src/main/java/com/georgemc2610/benzinapp/classes/listeners/ResponseMalfunctionListener.java
@@ -1,11 +1,22 @@
 package com.georgemc2610.benzinapp.classes.listeners;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.android.volley.Response;
+import com.georgemc2610.benzinapp.R;
+import com.georgemc2610.benzinapp.classes.Malfunction;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class ResponseMalfunctionListener implements Response.Listener
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.time.LocalDate;
+
+public class ResponseMalfunctionListener implements Response.Listener<String>
 {
     private final LinearLayout linearLayout;
     private final LayoutInflater inflater;
@@ -17,8 +28,50 @@ public class ResponseMalfunctionListener implements Response.Listener
     }
 
     @Override
-    public void onResponse(Object response)
+    public void onResponse(String response)
     {
+        linearLayout.removeAllViews();
 
+        try
+        {
+            // json response from the string response that was provided.
+            JSONArray JsonArrayResponse = new JSONArray(response);
+
+            for (int i = 0; i < JsonArrayResponse.length(); i++)
+            {
+                // get the card view
+                View v = inflater.inflate(R.layout.cardview_malfunction, null);
+
+                // card view text views
+                TextView titleView = v.findViewById(R.id.card_malfunction_title);
+                TextView at_kmView = v.findViewById(R.id.card_malfunction_at_km);
+                TextView dateView = v.findViewById(R.id.card_malfunction_date);
+                TextView statusView = v.findViewById(R.id.card_malfunction_status);
+
+                // card view buttons
+                FloatingActionButton deleteButton = v.findViewById(R.id.card_malfunction_button_delete);
+                FloatingActionButton editButton = v.findViewById(R.id.card_malfunction_button_edit);
+                FloatingActionButton fixButton = v.findViewById(R.id.card_malfunction_button_fix);
+
+                // get the data
+                JSONObject JsonObject = JsonArrayResponse.getJSONObject(i);
+
+                int id = JsonObject.getInt("id");
+                int at_km = JsonObject.getInt("at_km");
+                String title = JsonObject.getString("title");
+                String description = JsonObject.getString("description");
+                LocalDate started = LocalDate.parse(JsonObject.getString("started"));
+
+                // create instance of the malfunction class.
+                Malfunction malfunction = new Malfunction();
+
+                // add the view
+                linearLayout.addView(v);
+            }
+        }
+        catch (JSONException e)
+        {
+            System.out.println("Something went wrong. Exception message:\n" + e.getMessage());
+        }
     }
 }
