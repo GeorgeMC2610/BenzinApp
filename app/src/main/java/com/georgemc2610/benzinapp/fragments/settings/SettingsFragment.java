1891c56efcf507a60001c07ed9c7a94bed179936
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-12-08 11:36:52 +0200

    added language spinner

--- app/src/main/java/com/georgemc2610/benzinapp/fragments/settings/SettingsFragment.java
+++ app/src/main/java/com/georgemc2610/benzinapp/fragments/settings/SettingsFragment.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.georgemc2610.benzinapp.R;
 import com.georgemc2610.benzinapp.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment
 {
+    Spinner languageSpinner;
 
     private FragmentSettingsBinding binding;
 
@@ -20,6 +24,14 @@ public class SettingsFragment extends Fragment
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        languageSpinner = root.findViewById(R.id.settings_LanguageSpinner);
+
+        String[] options = new String[] { "English", "Ελληνικά" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, options);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
 
         return root;
     }
