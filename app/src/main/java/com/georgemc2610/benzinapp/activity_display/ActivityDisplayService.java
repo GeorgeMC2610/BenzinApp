9c6ee66dc8b4fed85e1998f283b6e61f6f8326de
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-12-08 21:14:14 +0200

    added support action bar in Activity Display

--- app/src/main/java/com/georgemc2610/benzinapp/activity_display/ActivityDisplayService.java
+++ app/src/main/java/com/georgemc2610/benzinapp/activity_display/ActivityDisplayService.java
@@ -3,6 +3,7 @@ package com.georgemc2610.benzinapp.activity_display;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.georgemc2610.benzinapp.R;
 
@@ -14,5 +15,31 @@ public class ActivityDisplayService extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_service);
+
+        // action bar with back button and correct title name.
+        try
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle("Service Data"); // TODO: Replace with string value
+        }
+        // if anything goes wrong, print it out.
+        catch (Exception e)
+        {
+            System.out.println("Something went wrong while trying to find Action Bar. Message: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
