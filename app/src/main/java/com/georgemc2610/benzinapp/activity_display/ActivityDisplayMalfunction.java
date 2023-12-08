b2f1316db087bef27d1c8487a55950a371e4ec9d
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-12-08 21:57:59 +0200

    added data to the malfunction activity

--- app/src/main/java/com/georgemc2610/benzinapp/activity_display/ActivityDisplayMalfunction.java
+++ app/src/main/java/com/georgemc2610/benzinapp/activity_display/ActivityDisplayMalfunction.java
@@ -2,19 +2,51 @@ package com.georgemc2610.benzinapp.activity_display;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.georgemc2610.benzinapp.R;
+import com.georgemc2610.benzinapp.classes.Malfunction;
+
+import java.util.Locale;
 
 public class ActivityDisplayMalfunction extends AppCompatActivity
 {
+    TextView titleView, descriptionView, startedView, endedView, atKmView, costView;
+    Malfunction malfunction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // initialize view
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_malfunction);
+
+        // get number format
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.getDefault());
+
+        // get text views
+        titleView = findViewById(R.id.text_view_display_malfunction_title);
+        descriptionView = findViewById(R.id.text_view_display_malfunction_desc);
+        startedView = findViewById(R.id.text_view_display_malfunction_date_started);
+        endedView = findViewById(R.id.text_view_display_malfunction_date_ended);
+        atKmView = findViewById(R.id.text_view_display_malfunction_discovered_at_km);
+        costView = findViewById(R.id.text_view_display_malfunction_cost);
+
+        // get the serializable object
+        malfunction = (Malfunction) getIntent().getSerializableExtra("malfunction");
+
+        // set required views content
+        titleView.setText(malfunction.getTitle());
+        descriptionView.setText(malfunction.getDescription());
+        startedView.setText(malfunction.getStarted().toString());
+        atKmView.setText(numberFormat.format(malfunction.getAt_km()) + " " + getString(R.string.km_short));
+
+        // set optional views content
+        endedView.setText(malfunction.getEnded() == null? "-" : malfunction.getEnded().toString());
+        costView.setText(malfunction.getCost() == 0f? "-" : String.valueOf(malfunction.getCost()));
 
         // action bar with back button and correct title name.
         try
