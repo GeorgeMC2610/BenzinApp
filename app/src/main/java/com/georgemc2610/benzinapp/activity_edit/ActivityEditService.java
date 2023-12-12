39436194e20c8ed1c1f095fd21bb09e3a755f4e2
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-12-12 02:17:32 +0200

    you know all that

--- app/src/main/java/com/georgemc2610/benzinapp/activity_edit/ActivityEditService.java
+++ app/src/main/java/com/georgemc2610/benzinapp/activity_edit/ActivityEditService.java
@@ -3,6 +3,7 @@ package com.georgemc2610.benzinapp.activity_edit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.georgemc2610.benzinapp.R;
 
@@ -12,7 +13,34 @@ public class ActivityEditService extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // initialize activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_service);
+
+        // action bar with back button and correct title name.
+        try
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle(getString(R.string.title_edit_record));
+        }
+        // if anything goes wrong, print it out.
+        catch (Exception e)
+        {
+            System.out.println("Something went wrong while trying to find Action Bar. Message: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // once the back button is pressed, close the activity.
+        if (item.getItemId() == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
