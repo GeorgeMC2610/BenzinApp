e06a5d0a5023fbcc1ab3d2edc9e800a3d63ab00f
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-12-12 02:56:35 +0200

    modifying spinner in settings

--- app/src/main/java/com/georgemc2610/benzinapp/fragments/settings/SettingsFragment.java
+++ app/src/main/java/com/georgemc2610/benzinapp/fragments/settings/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -15,8 +16,7 @@ import com.georgemc2610.benzinapp.R;
 import com.georgemc2610.benzinapp.classes.RequestHandler;
 import com.georgemc2610.benzinapp.databinding.FragmentSettingsBinding;
 
-public class SettingsFragment extends Fragment implements View.OnClickListener
-{
+public class SettingsFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     Spinner languageSpinner;
     Button LogoutButton;
 
@@ -30,7 +30,7 @@ public class SettingsFragment extends Fragment implements View.OnClickListener
 
         languageSpinner = root.findViewById(R.id.settings_LanguageSpinner);
 
-        String[] options = new String[] { "English", "Ελληνικά" };
+        String[] options = new String[] { getString(), ""English", "Ελληνικά" };
 
         LogoutButton = root.findViewById(R.id.settings_LogoutButton);
         LogoutButton.setOnClickListener(this);
@@ -39,6 +39,8 @@ public class SettingsFragment extends Fragment implements View.OnClickListener
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, options);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         languageSpinner.setAdapter(adapter);
+
+        languageSpinner.setOnItemSelectedListener(this);
 
         return root;
     }
@@ -54,5 +56,20 @@ public class SettingsFragment extends Fragment implements View.OnClickListener
     public void onClick(View v)
     {
         RequestHandler.getInstance().Logout(getActivity());
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        if (position == 0)
+        {
+            System.out.println("ENGLISH!!");
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
     }
 }
