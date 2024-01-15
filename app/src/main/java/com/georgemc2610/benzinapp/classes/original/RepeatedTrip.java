5e1bc9cea21371dd8dc5a1eaa01845928dd984c9
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-01-15 15:03:36 +0200

    added repeated trip class

---
+++ app/src/main/java/com/georgemc2610/benzinapp/classes/original/RepeatedTrip.java
@@ -0,0 +1,6 @@
+package com.georgemc2610.benzinapp.classes.original;
+
+public class RepeatedTrip
+{
+
+}
--- app/src/main/java/com/georgemc2610/benzinapp/classes/requests/DataHolder.java
+++ app/src/main/java/com/georgemc2610/benzinapp/classes/requests/DataHolder.java
@@ -3,6 +3,7 @@ package com.georgemc2610.benzinapp.classes.requests;
 import com.georgemc2610.benzinapp.classes.original.Car;
 import com.georgemc2610.benzinapp.classes.original.FuelFillRecord;
 import com.georgemc2610.benzinapp.classes.original.Malfunction;
+import com.georgemc2610.benzinapp.classes.original.RepeatedTrip;
 import com.georgemc2610.benzinapp.classes.original.Service;
 
 import java.util.List;
@@ -29,6 +30,7 @@ public class DataHolder
     public List<FuelFillRecord> records;
     public List<Malfunction> malfunctions;
     public List<Service> services;
+    public List<RepeatedTrip> trips;
     public Car car;
 }
 
