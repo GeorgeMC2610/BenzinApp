e4d18df9b05f39313aebc2ca17561035f9c1fbb8
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-01-15 17:18:04 +0200

    repeated trip activity add

---
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityAddRepeatedTrip.java
@@ -0,0 +1,16 @@
+package com.georgemc2610.benzinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAddRepeatedTrip extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_repeated_trip);
+    }
+}
