53632b9792f0c43d587a9ef28aa84b9afe092b72
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-01-17 14:58:55 +0200

    added button listener for trips redirection

---
+++ app/src/main/java/com/georgemc2610/benzinapp/classes/listeners/ButtonRedirectToAddTripActivityListener.java
@@ -0,0 +1,25 @@
+package com.georgemc2610.benzinapp.classes.listeners;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import com.georgemc2610.benzinapp.acitvity_add.ActivityAddRepeatedTrip;
+
+public class ButtonRedirectToAddTripActivityListener implements View.OnClickListener
+{
+    private final Context context;
+
+    public ButtonRedirectToAddTripActivityListener(Context context)
+    {
+        this.context = context;
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+        Intent intent = new Intent(context, ActivityAddRepeatedTrip.class);
+        context.startActivity(intent);
+    }
+}
