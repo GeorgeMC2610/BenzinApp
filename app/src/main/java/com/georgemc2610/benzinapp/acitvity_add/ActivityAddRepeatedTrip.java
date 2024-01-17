27d76b527b871e9f64dcb6efc24f1ac046c383cd
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-01-17 15:12:05 +0200

    views retrieval

--- app/src/main/java/com/georgemc2610/benzinapp/acitvity_add/ActivityAddRepeatedTrip.java
+++ app/src/main/java/com/georgemc2610/benzinapp/acitvity_add/ActivityAddRepeatedTrip.java
@@ -6,17 +6,32 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.georgemc2610.benzinapp.R;
 
 public class ActivityAddRepeatedTrip extends AppCompatActivity
 {
+    private EditText title, timesRepeating;
+    private CheckBox isRepeating;
+    private TextView origin, destination, totalKm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         // initialize activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_repeated_trip);
+
+        // views
+        title = findViewById(R.id.repeated_trips_edit_text_title);
+        timesRepeating = findViewById(R.id.repeated_trips_edit_text_times_repeating);
+        isRepeating = findViewById(R.id.repeated_trips_checkbox_not_repeating);
+        origin = findViewById(R.id.repeated_trips_text_view_origin);
+        destination = findViewById(R.id.repeated_trips_text_view_destination);
+        totalKm = findViewById(R.id.repeated_trips_text_view_kilometers);
 
         // action bar
         try
