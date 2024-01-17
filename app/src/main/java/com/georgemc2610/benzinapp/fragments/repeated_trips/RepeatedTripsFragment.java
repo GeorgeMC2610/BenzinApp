71856bcf7320f3589fc2f6b5e1dd8c0385f8897e
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-01-17 15:05:12 +0200

    added logic for opening add trip activity

--- app/src/main/java/com/georgemc2610/benzinapp/fragments/repeated_trips/RepeatedTripsFragment.java
+++ app/src/main/java/com/georgemc2610/benzinapp/fragments/repeated_trips/RepeatedTripsFragment.java
@@ -8,18 +8,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.georgemc2610.benzinapp.R;
+import com.georgemc2610.benzinapp.classes.listeners.ButtonRedirectToAddTripActivityListener;
 import com.georgemc2610.benzinapp.databinding.FragmentRepeatedTripsBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class RepeatedTripsFragment extends Fragment
 {
 
     private FragmentRepeatedTripsBinding binding;
+    private FloatingActionButton buttonAddTrip;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        // initialize fragment.
         binding = FragmentRepeatedTripsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // get button and set listener for redirection
+        buttonAddTrip = root.findViewById(R.id.button_add);
+        buttonAddTrip.setOnClickListener(new ButtonRedirectToAddTripActivityListener(getContext()));
 
         return root;
     }
