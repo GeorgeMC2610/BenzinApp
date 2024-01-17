7a274d9ff01a9830190bf8608d763ebe55338c5c
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-01-17 15:07:09 +0200

    added support action bar

--- app/src/main/java/com/georgemc2610/benzinapp/acitvity_add/ActivityAddRepeatedTrip.java
+++ app/src/main/java/com/georgemc2610/benzinapp/acitvity_add/ActivityAddRepeatedTrip.java
@@ -8,11 +8,24 @@ import com.georgemc2610.benzinapp.R;
 
 public class ActivityAddRepeatedTrip extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // initialize activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_repeated_trip);
+
+        // action bar
+        try
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle(getString(R.string.title_add_trip));
+        }
+        // if anything goes wrong, print it out.
+        catch (Exception e)
+        {
+            System.out.println("Something went wrong while trying to find Action Bar. Message: " + e.getMessage());
+        }
     }
 }
