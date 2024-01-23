098b1f44aae11d9f3de2e209bd4150c2c3f0a5ce
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-01-23 17:48:31 +0200

    activity edit repeated trip

--- app/src/main/java/com/georgemc2610/benzinapp/activity_edit/ActivityEditRepeatedTrip.java
+++ app/src/main/java/com/georgemc2610/benzinapp/activity_edit/ActivityEditRepeatedTrip.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ActivityEditRepeatedTrip extends AppCompatActivity {
+import com.georgemc2610.benzinapp.R;
+
+public class ActivityEditRepeatedTrip extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_repeated_trip);
     }
