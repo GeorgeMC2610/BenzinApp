fe01754590c533c3d7c39ca1fe5ca00a3f26f0e5
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-02-02 12:19:06 +0200

    text views in activity display

--- app/src/main/java/com/georgemc2610/benzinapp/activity_display/ActivityDisplayRepeatedTrip.java
+++ app/src/main/java/com/georgemc2610/benzinapp/activity_display/ActivityDisplayRepeatedTrip.java
@@ -3,16 +3,28 @@ package com.georgemc2610.benzinapp.activity_display;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.georgemc2610.benzinapp.R;
 
 public class ActivityDisplayRepeatedTrip extends AppCompatActivity
 {
+    TextView title, timesRepeating, trip, km, cost, lt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // initialize activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_repeated_trip);
+
+        // get views
+        title = findViewById(R.id.display_repeated_trip_text_view_title);
+        timesRepeating = findViewById(R.id.display_repeated_trip_text_view_times_repeating);
+        trip = findViewById(R.id.display_repeated_trip_text_view_from_origin_to_destination);
+        km = findViewById(R.id.display_repeated_trip_text_view_total_km);
+        cost = findViewById(R.id.display_repeated_trip_text_view_total_cost);
+        lt = findViewById(R.id.display_repeated_trip_text_view_total_lt);
     }
 }
