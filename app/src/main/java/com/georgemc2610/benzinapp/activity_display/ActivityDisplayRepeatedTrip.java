46de5961d2202a523bc1c8d455345a59f6a0aa97
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-02-02 13:34:01 +0200

    displaying data

--- app/src/main/java/com/georgemc2610/benzinapp/activity_display/ActivityDisplayRepeatedTrip.java
+++ app/src/main/java/com/georgemc2610/benzinapp/activity_display/ActivityDisplayRepeatedTrip.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.georgemc2610.benzinapp.R;
+import com.georgemc2610.benzinapp.classes.original.RepeatedTrip;
+import com.georgemc2610.benzinapp.classes.requests.DataHolder;
 
 public class ActivityDisplayRepeatedTrip extends AppCompatActivity
 {
     TextView title, timesRepeating, trip, km, cost, lt;
-
+    RepeatedTrip repeatedTrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,5 +28,20 @@ public class ActivityDisplayRepeatedTrip extends AppCompatActivity
         km = findViewById(R.id.display_repeated_trip_text_view_total_km);
         cost = findViewById(R.id.display_repeated_trip_text_view_total_cost);
         lt = findViewById(R.id.display_repeated_trip_text_view_total_lt);
+
+        // get the serializable object
+        repeatedTrip = (RepeatedTrip) getIntent().getSerializableExtra("repeated_trip");
+
+        // PREPEI NA TA KANW KALYTERA, ALLA AYTO THA GINEI ARGOTERA OK?
+
+        // set the views' texts
+        title.setText(repeatedTrip.getTitle());
+        timesRepeating.setText("Repeating " + repeatedTrip.getTimesRepeating() + " times per week.");
+        trip.setText(repeatedTrip.getOrigin());
+        km.setText(repeatedTrip.getTotalKm() + " km trip");
+        cost.setText("€" + repeatedTrip.getTotalCostEur(DataHolder.getInstance().car) * repeatedTrip.getTimesRepeating() + "()");
+        lt.setText(repeatedTrip.getTotalLt(DataHolder.getInstance().car) * repeatedTrip.getTimesRepeating() + " lt ()");
     }
+
+
 }
