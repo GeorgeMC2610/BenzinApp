8bf45fc0083fb3a1bdc760c2bb90a83159dc2a73
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-03-03 01:21:22 +0200

    added edit account activity

---
+++ app/src/main/java/com/georgemc2610/benzinapp/ActivityEditAccount.java
@@ -0,0 +1,16 @@
+package com.georgemc2610.benzinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityEditAccount extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_account);
+    }
+}
