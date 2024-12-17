82d9462afeefe45b072d03066feaf345b17d6e0b
Giorgos Seimenis <giorgosseimenis@gmail.com>
2024-12-18 00:56:14 +0200

    removed project files from benzinapp java

--- app/src/main/java/com/georgemc2610/benzinapp/classes/listeners/CoordinatesChangeListener.java
+++
@@ -1,6 +0,0 @@
-package com.georgemc2610.benzinapp.classes.listeners;
-
-public interface CoordinatesChangeListener
-{
-    void deleteCoordinates();
-}
--- app/src/main/java/com/georgemc2610/benzinapp/classes/requests/RequestState.java
+++
@@ -1,8 +0,0 @@
-package com.georgemc2610.benzinapp.classes.requests;
-
-public enum RequestState
-{
-    LOGGED_OUT,
-    REQUESTS_SENT,
-    OPENED_ACTIVITY
-}
