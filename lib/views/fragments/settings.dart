646f713c62f1be0fe6057df94d167b73648cc950
Giorgos Seimenis <giorgosseimenis@gmail.com>
2025-01-19 21:09:34 +0200

    settings fragment refined

--- lib/views/fragments/settings.dart
+++ lib/views/fragments/settings.dart
@@ -11,20 +11,98 @@ class _SettingsFragmentState extends State<SettingsFragment> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            'You have pushed the button this many times:',
-          ),
-          Text(
-            'Settings Fragment',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+          child:
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Application Appearance",
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500
+                    )
+                ),
+
+                const SizedBox(height: 12),
+
+                const ListTile(
+                  title: Text("Dark Mode"),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  leading: Icon(Icons.dark_mode),
+                ),
+                ListTile(
+                  title: const Text("Language"),
+                  onTap: () {
+                  },
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  leading: const Icon(Icons.language_outlined),
+                ),
+
+                const SizedBox(height: 30),
+
+                const Text("Account Settings",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500
+                  )
+                ),
+
+                const SizedBox(height: 12),
+
+                const ListTile(
+                  title: Text("Fast Login"),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  leading: Icon(Icons.fast_forward),
+                ),
+                const ListTile(
+                  title: Text("Logout"),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  leading: Icon(Icons.logout),
+                ),
+                const ListTile(
+                  title: Text("Edit Account"),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  leading: Icon(Icons.edit),
+                ),
+
+                const SizedBox(height: 30),
+
+                const Text("About the App",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500
+                  )
+                ),
+
+                const SizedBox(height: 12),
+
+                const ListTile(
+                  title: Text("Privacy Policy"),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  leading: Icon(Icons.security_outlined),
+                ),
+                const ListTile(
+                  title: Text("Terms and Conditions"),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  leading: Icon(Icons.newspaper),
+                ),
+                ListTile(
+                  title: const Text("Technical Information"),
+                  onTap: () {
+                  },
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  leading: const Icon(Icons.developer_mode),
+                ),
+              ])
       ),
     );
+
+
   }
 
 }
