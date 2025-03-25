bcf28250984a6fa64ba9671443752b51559e55c9
Georgios Seimenis <giorgosseimenis@gmail.com>
2025-03-25 16:06:23 +0200

    added the car class, which contains basic data

---
+++ lib/services/classes/car.dart
@@ -0,0 +1,16 @@
+class Car {
+
+  Car({
+    required this.id,
+    required this.username,
+    required this.manufacturer,
+    required this.model,
+    required this.year
+  });
+
+  final int id;
+  final String username;
+  final String manufacturer;
+  final String model;
+  final int year;
+}
