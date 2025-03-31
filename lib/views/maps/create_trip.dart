826e9a0f17a2f95f551cbc1fd3f189a1992cdaa5
Giorgos Seimenis <giorgosseimenis@gmail.com>
2025-03-31 21:38:58 +0300

    creating the create trips view

---
+++ lib/views/maps/create_trip.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class CreateTrip extends StatefulWidget {
+  const CreateTrip({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CreateTripState();
+}
+
+class _CreateTripState extends State<CreateTrip> {
+  
+
+}
