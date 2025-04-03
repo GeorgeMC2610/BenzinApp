ee7cc55574f02d5e72325c9c8b7d6e0d43e69189
Georgios Seimenis <giorgosseimenis@gmail.com>
2025-04-03 15:27:39 +0300

    repeated trip card

--- lib/views/shared/cards/trip_card.dart
+++ lib/views/shared/cards/trip_card.dart
@@ -1,5 +1,8 @@
 import 'package:benzinapp/services/classes/trip.dart';
+import 'package:benzinapp/services/locale_string_converter.dart';
+import 'package:benzinapp/views/forms/trip.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -21,7 +24,10 @@ class _TripCardState extends State<TripCard> {
 
   @override
   Widget build(BuildContext context) {
-    final NumberFormat format = NumberFormat('#,###', Provider.of<LanguageProvider>(context).currentLocale.toLanguageTag());
+    final NumberFormat format = NumberFormat('#,###', Provider
+        .of<LanguageProvider>(context)
+        .currentLocale
+        .toLanguageTag());
 
     return ListTile(
       onTap: () {
@@ -31,12 +37,16 @@ class _TripCardState extends State<TripCard> {
       title: Text(
         widget.trip.title,
         style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 18
+            fontWeight: FontWeight.bold,
+            fontSize: 18
         ),
       ),
 
-      style: ListTileTheme.of(context).style,
+      subtitle: getSubtitle(),
+
+      style: ListTileTheme
+          .of(context)
+          .style,
 
       // --- === EDIT AND DELETE BUTTONS === ---
       trailing: Row(
@@ -48,15 +58,17 @@ class _TripCardState extends State<TripCard> {
           FloatingActionButton.small(
             heroTag: null,
             onPressed: () {
-              /*Navigator.push(
+              Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => ServiceForm(service: widget.service)
+                      builder: (context) => TripForm(trip: widget.trip)
                   )
-              );*/
+              );
             },
             elevation: 0,
-            backgroundColor: Theme.of(context).primaryColor,
+            backgroundColor: Theme
+                .of(context)
+                .primaryColor,
             foregroundColor: Colors.white,
             child: const Icon(Icons.edit),
           ),
@@ -69,9 +81,9 @@ class _TripCardState extends State<TripCard> {
                   context,
                   'Delete Trip',
                       (Function(bool) setLoadingState) {
-
                     RequestHandler.sendDeleteRequest(
-                      '${DataHolder.destination}/repeated_trip/${widget.trip.id}',
+                      '${DataHolder.destination}/repeated_trip/${widget.trip
+                          .id}',
                           () {
                         setLoadingState(true); // Close the dialog
                       },
@@ -90,6 +102,99 @@ class _TripCardState extends State<TripCard> {
         ],
       ),
 
+    );
+  }
+
+  Widget getSubtitle() {
+    if (widget.trip.timesRepeating == 1) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const Icon(FontAwesomeIcons.arrowRight, size: 15,),
+              const SizedBox(width: 5),
+              const Text(" One-time trip", )
+            ],
+          ),
+
+          const SizedBox(height: 5),
+
+          Row(
+            children: [
+              const Icon(FontAwesomeIcons.coins, size: 18,),
+              const SizedBox(width: 5),
+              Text(" €19.99", style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+              )),
+            ],
+          ),
+
+          const SizedBox(height: 5),
+
+          Row(
+            children: [
+              const Icon(FontAwesomeIcons.car, size: 15,),
+              const SizedBox(width: 5),
+              Text(" ${LocaleStringConverter.formattedDouble(context, widget.trip.totalKm)} km")
+            ],
+          ),
+
+          Text("Created at: ${LocaleStringConverter.dateShortDayMonthYearString(context, widget.trip.created)}")
+        ],
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            const Icon(FontAwesomeIcons.arrowRotateRight, size: 15,),
+            const SizedBox(width: 5),
+            Text(" ${widget.trip.timesRepeating} times per week", )
+          ],
+        ),
+
+        const SizedBox(height: 5),
+
+        Row(
+          children: [
+            const Icon(FontAwesomeIcons.coins, size: 18,),
+            const SizedBox(width: 5),
+            Text(" €19.99 per week", style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+            )),
+          ],
+        ),
+
+        Row(
+          children: [
+            const Icon(FontAwesomeIcons.gasPump, size: 18,),
+            const SizedBox(width: 5),
+            Text(" 5.20 lt. per week", style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold
+            )),
+          ],
+        ),
+
+        const SizedBox(height: 5),
+
+        Row(
+          children: [
+            const Icon(FontAwesomeIcons.car, size: 15,),
+            const SizedBox(width: 5),
+            Text(" ${LocaleStringConverter.formattedDouble(context, widget.trip.totalKm)} km")
+          ],
+        ),
+
+        Text("Created at: ${LocaleStringConverter.dateShortDayMonthYearString(context, widget.trip.created)}")
+      ],
     );
   }
 
