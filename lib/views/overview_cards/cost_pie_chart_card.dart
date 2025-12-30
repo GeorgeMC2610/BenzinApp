cbc333c4e415680110a488769366775b0e8a2b64
Giorgos Seimenis <>
2025-12-30 14:38:54 +0200

    cost pie chart card is now loading correctly

--- lib/views/overview_cards/cost_pie_chart_card.dart
+++ lib/views/overview_cards/cost_pie_chart_card.dart
@@ -1,5 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:benzinapp/services/classes/car.dart';
+import 'package:benzinapp/services/managers/fuel_fill_record_manager.dart';
+import 'package:benzinapp/services/managers/malfunction_manager.dart';
+import 'package:benzinapp/services/managers/service_manager.dart';
+import 'package:benzinapp/views/shared/cards/loading_data_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_translate/flutter_translate.dart';
@@ -17,20 +21,6 @@ class CostPieChartCard extends StatefulWidget {
 class _CostPieChartCardState extends State<CostPieChartCard> {
 
   double? totalServiceCosts, totalMalfunctionCosts, totalFuelFillCosts;
-
-  @override
-  void initState() {
-    super.initState();
-    initialize();
-  }
-
-  initialize() async {
-    setState(() {
-      totalServiceCosts = Car.getTotalServiceCosts();
-      totalMalfunctionCosts = Car.getTotalMalfunctionCosts();
-      totalFuelFillCosts = Car.getTotalFuelFillCosts();
-    });
-  }
 
   Widget normalBody() => SizedBox(
     width: MediaQuery.sizeOf(context).width,
@@ -116,6 +106,18 @@ class _CostPieChartCardState extends State<CostPieChartCard> {
   );
 
   @override
-  Widget build(BuildContext context) => normalBody();
+  Widget build(BuildContext context) => Consumer3<FuelFillRecordManager, MalfunctionManager, ServiceManager>(
+    builder: (context, fuelManager, malfunctionManager, serviceManager, _) {
+      if (fuelManager.local == null || malfunctionManager.local == null || serviceManager.local == null) {
+        return const LoadingDataCard();
+      }
+
+      totalServiceCosts = Car.getTotalServiceCosts();
+      totalMalfunctionCosts = Car.getTotalMalfunctionCosts();
+      totalFuelFillCosts = Car.getTotalFuelFillCosts();
+
+      return normalBody();
+    },
+  );
 
 }
