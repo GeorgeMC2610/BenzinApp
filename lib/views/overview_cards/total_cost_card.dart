429ae9ab53341d0b72de99c9e23780f4b1969edb
Giorgos Seimenis <>
2026-03-15 01:25:14 +0200

    converted consumption values

--- lib/views/overview_cards/total_cost_card.dart
+++ lib/views/overview_cards/total_cost_card.dart
@@ -1,6 +1,9 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:benzinapp/services/classes/car.dart';
+import 'package:benzinapp/services/distance_metric_provider.dart';
+import 'package:benzinapp/services/fuel_metric_service.dart';
 import 'package:benzinapp/services/managers/car_manager.dart';
+import 'package:benzinapp/services/volume_metric_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:provider/provider.dart';
@@ -22,76 +25,85 @@ class _TotalCostCardContainerState extends State<TotalCostCardContainer> {
 
   double? totalEfficiency, totalConsumption, totalTravelCost;
 
-  Widget normalBody() => SizedBox(
-    width: MediaQuery.sizeOf(context).width,
-    child: Card(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0)
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                  child: AutoSizeText(
-                      translate('averageConsumption'),
-                      maxLines: 1,
-                      maxFontSize: 25,
-                      style: const TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold
+  Widget normalBody() {
+    final distanceProvider = Provider.of<DistanceMetricProvider>(context);
+    final volumeProvider = Provider.of<VolumeMetricProvider>(context);
+
+    final convertedConsumption = FuelMetricService.getConvertedConsumption(totalConsumption!, distanceProvider.metric, volumeProvider.metric);
+    final convertedEfficiency = FuelMetricService.getConvertedEfficiency(totalEfficiency!, distanceProvider.metric, volumeProvider.metric);
+    final convertedTravelCost = FuelMetricService.getConvertedTravelCost(totalTravelCost!, distanceProvider.metric);
+
+    return SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                      child: AutoSizeText(
+                          translate('averageConsumption'),
+                          maxLines: 1,
+                          maxFontSize: 25,
+                          style: const TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold
+                          )
                       )
-                  )
-              ),
+                  ),
 
-              const SizedBox(height: 15),
+                  const SizedBox(height: 15),
 
-              Text(translate('consumption'),
-                style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-              Text('${LocaleStringConverter.formattedDouble(context, totalConsumption!)} lt./100 km',
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
-              ),
+                  Text(translate('consumption'),
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text('${LocaleStringConverter.formattedDouble(context, convertedConsumption)} ${FuelMetricService.getConsumptionLabel(distanceProvider.metric, volumeProvider.metric)}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
 
-              const SizedBox(height: 10),
+                  const SizedBox(height: 10),
 
-              Text(translate('efficiency'),
-                style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-              Text('${LocaleStringConverter.formattedDouble(context, totalEfficiency!)} km/lt.',
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
-              ),
+                  Text(translate('efficiency'),
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text('${LocaleStringConverter.formattedDouble(context, convertedEfficiency)} ${FuelMetricService.getEfficiencyLabel(distanceProvider.metric, volumeProvider.metric)}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
 
-              const SizedBox(height: 10),
+                  const SizedBox(height: 10),
 
-              Text(translate('travel_cost'),
-                style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                ),
+                  Text(translate('travel_cost'),
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text('${CarManager().watchingCar!.toCurrency(LocaleStringConverter.formattedDouble(context, convertedTravelCost))}/${distanceProvider.label}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
               ),
-              Text('${CarManager().watchingCar!.toCurrency(LocaleStringConverter.formattedDouble(context, totalTravelCost!))}/km',
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-            ],
-          ),
+            )
         )
-    ),
-  );
+    );
+  }
 
   @override
   Widget build(BuildContext context) => Consumer3<FuelFillRecordManager, MalfunctionManager, ServiceManager>(
