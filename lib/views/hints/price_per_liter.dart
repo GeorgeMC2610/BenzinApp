03b97bc5a3450d221ba960456c86c549b3e29e56
Georgios Seimenis <giorgosseimenis@gmail.com>
2026-05-08 20:55:59 +0300

    all hints were properly added and analyzed

--- lib/views/hints/price_per_liter.dart
+++ lib/views/hints/price_per_liter.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 
 class PricePerLiter extends StatelessWidget {
   const PricePerLiter({super.key});
 
   @override
-  Widget build(BuildContext context) => const Column(
-    children: [
-      Text("Booyah from PRICE PER LITER!")
-    ],
+  Widget build(BuildContext context) => SingleChildScrollView(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(translate('gasPriceTitle'), style: Theme.of(context).textTheme.titleLarge),
+        Text(translate('gasPriceBody'),
+            style: Theme.of(context).textTheme.bodyMedium
+        ),
+
+        Image.asset('assets/images/gas_price.jpg'),
+
+        const SizedBox(height: 25),
+
+      ],
+    ),
   );
 
 }
