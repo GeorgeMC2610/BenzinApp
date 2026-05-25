0e9ed2b96c4a759043c0b8dd920924019ae4842a
Georgios Seimenis <giorgosseimenis@gmail.com>
2026-05-25 12:21:31 +0300

    fcm manager added

--- lib/services/managers/fcm_manager.dart
+++ lib/services/managers/fcm_manager.dart
@@ -1,37 +1,26 @@
-import 'package:benzinapp/views/shared/notification.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:http/http.dart' as http;
+import 'package:benzinapp/services/data_holder.dart';
+import 'package:benzinapp/services/request_handler.dart';
 
 class FCMManager {
-  final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+  static String baseUrl = DataHolder.destination;
 
-  Future<void> init() async {
-    // 1. Request Permission (Crucial for iOS)
-    NotificationSettings settings = await _fcm.requestPermission(
-      alert: true,
-      badge: true,
-      sound: true,
-    );
-
-    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-      // 2. Get Token
-      String? token = await _fcm.getToken();
-      if (token != null) {
-        await _sendTokenToBackend(token);
-      }
-
-      // 3. Listen for Token Refresh
-      _fcm.onTokenRefresh.listen(_sendTokenToBackend);
-    }
+  Future<bool> isSubscribed({String? fcmToken}) async {
+    final String url = "$baseUrl/is_subscribed?token=$fcmToken";
+    final response = await RequestHandler.sendGetRequest(url);
+    return response.ok;
   }
 
-  Future<void> _sendTokenToBackend(String token) async {
-    // TODO: Replace with your actual backend call logic
-    // Usually, you'd send this along with the User ID/Auth header
-    SnackbarNotification.show(MessageType.info, token);
-    print("FCM Token: $token");
-    // Example:
-    // await http.post(Uri.parse('https://your-api.com/user/fcm-token'),
-    //    body: {'token': token});
+  Future<void> subscribe(String fcmToken) async {
+    final String url = "$baseUrl/subscribe_notifications";
+    await RequestHandler.sendPostRequest(url, true, {
+      'token': fcmToken
+    });
+  }
+
+  Future<void> unsubscribe(String fcmToken) async {
+    final String url = "$baseUrl/unsubscribe_notifications";
+    await RequestHandler.sendDeleteRequest(url, body: {
+      'token': fcmToken
+    });
   }
 }
