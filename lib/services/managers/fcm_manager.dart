12ada273689c607c10692ae503ab4d161eb15aae
Georgios Seimenis <giorgosseimenis@gmail.com>
2026-05-25 13:18:18 +0300

    added values for FCM sending

--- lib/services/managers/fcm_manager.dart
+++ lib/services/managers/fcm_manager.dart
@@ -1,8 +1,32 @@
 import 'package:benzinapp/services/data_holder.dart';
 import 'package:benzinapp/services/request_handler.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class FCMManager {
+import '../notifications/firebase_messaging_service.dart';
+
+class FCMManager with ChangeNotifier {
+  static final FCMManager _instance = FCMManager._internal();
+  factory FCMManager() => _instance;
+  FCMManager._internal() {
+    loadSettings();
+  }
+
   static String baseUrl = DataHolder.destination;
+
+  bool _receiveEmailNotifications = false;
+  bool _receivePushNotifications = false;
+
+  bool get receiveEmailNotifications => _receiveEmailNotifications;
+  bool get receivePushNotifications => _receivePushNotifications;
+
+  Future<void> loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    _receiveEmailNotifications = prefs.getBool('receiveEmailNotifications') ?? false;
+    _receivePushNotifications = prefs.getBool('receivePushNotifications') ?? false;
+    notifyListeners();
+  }
 
   Future<bool> isSubscribed({String? fcmToken}) async {
     final String url = "$baseUrl/is_subscribed?token=$fcmToken";
@@ -22,5 +46,36 @@ class FCMManager {
     await RequestHandler.sendDeleteRequest(url, body: {
       'token': fcmToken
     });
+  }
+
+  Future<void> toggleEmailNotifications(bool value) async {
+    _receiveEmailNotifications = value;
+    notifyListeners();
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('receiveEmailNotifications', value);
+
+    // TODO: Implement remote email notification subscription if endpoint exists
+  }
+
+  Future<void> togglePushNotifications(bool value) async {
+    _receivePushNotifications = value;
+    notifyListeners();
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('receivePushNotifications', value);
+
+    if (value) {
+      await FirebaseMessagingService.instance().requestPermission();
+      final token = await FirebaseMessaging.instance.getToken();
+      if (token != null) {
+        await subscribe(token);
+      }
+    } else {
+      final token = await FirebaseMessaging.instance.getToken();
+      if (token != null) {
+        await unsubscribe(token);
+      }
+    }
   }
 }
